d83e9be88ee4a3c7620e4f12765a71d279ed5ff9
Kawsar Hamid <59446095+KawsarHamid99@users.noreply.github.com>
2024-05-20 21:29:15 +0600

    channel Practice

--- channels/ChannelsPractice/myapp/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import consumer
-
-websocket_urlpatterns=[
-    path('ws/sc/',consumer.MySyncConsumer.as_asgi()),
-    path('ws/ac/',consumer.MyAsyncConsumer.as_asgi())
-]
